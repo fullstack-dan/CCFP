72f2657d301cede695024ee08d21e2a6681a4b4c
ClayMorton <mmorton1163@gmail.com>
2024-12-02 17:53:07 -0500

    adding the file :P

---
+++ Backend/dialogflow-server/gemini.py
@@ -0,0 +1,24 @@
+from google.cloud import language_v2
+import google.generativeai as genai
+
+# API key setup
+genai.configure(api_key='API KEY GOES HERE')
+# set gemini model
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+# Functions
+###################################################################################################
+
+def talk_to_gemini(user_input, server_response):
+    ''' 
+        This function takes the user input and passes it to gemini using a
+        specially engineered prmopt and returns something in natural language 
+    '''
+    response = model.generate_content("Our user said: \"" + user_input + "\" \
+and Our database responded with this: \"" + server_response + "\" using what \
+the server said, generate a response to our user that gives them a clear \
+understanding and a thorough response to their initial question. If the server \
+did not provide a reply or gave an invalid reuest message, you may simply reply \
+with an aplogetic message about not understanding what the user is asking and \
+saying we can't answer that question. Keep your response limited to 200-300 words.")
+    return response
